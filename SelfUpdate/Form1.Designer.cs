fd5ad7192aa48d496e5aa1c5b94fe80e8b5344e1
AliyaZa <zainullinaaliya45@gmail.com>
2019-09-08 18:02:23 +0300

    Add files

---
+++ SelfUpdate/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SelfUpdate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Renovate = new System.Windows.Forms.Button();
+            this.textBox_Version = new System.Windows.Forms.TextBox();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Renovate
+            // 
+            this.Renovate.Location = new System.Drawing.Point(102, 226);
+            this.Renovate.Name = "Renovate";
+            this.Renovate.Size = new System.Drawing.Size(75, 23);
+            this.Renovate.TabIndex = 0;
+            this.Renovate.Text = "Обновить";
+            this.Renovate.UseVisualStyleBackColor = true;
+            this.Renovate.Click += new System.EventHandler(this.Renovate_Click);
+            // 
+            // textBox_Version
+            // 
+            this.textBox_Version.Location = new System.Drawing.Point(12, 56);
+            this.textBox_Version.Name = "textBox_Version";
+            this.textBox_Version.Size = new System.Drawing.Size(260, 20);
+            this.textBox_Version.TabIndex = 1;
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(197, 226);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 2;
+            this.Exit.Text = "Выход";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.textBox_Version);
+            this.Controls.Add(this.Renovate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Renovate;
+        private System.Windows.Forms.TextBox textBox_Version;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ SelfUpdate/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace SelfUpdate
+{
+    public partial class Form1 : Form
+    {
+        /*[DllImport("SelfUpdate.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        static extern IntPtr BeginUpdateResourse(
+            [In] string pFileName,
+            [In] bool bDeleteExistingResources
+            );
+        [DllImport("SelfUpdate.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool UpdateResource(
+            [In] IntPtr hUpdate,
+            [In] IntPtr lpType,
+            [In] IntPtr lpName,
+            [In] ushort wLanguage,
+            [In, Optional] string lpData,
+            [In] int cbData
+            );
+        [DllImport("SelfUpdate.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool EndUpdateResource(
+            [In] IntPtr hUpdate,
+            [In] bool fDiscard
+            );
+
+        static readonly IntPtr VERSION_INFO_ID = (IntPtr)16;
+        static readonly IntPtr VERSION_NAME_ID = (IntPtr)1;
+        const ushort VERSION_LANG_ID = 4105;*/
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            var CurrentVersion = FileVersionInfo.GetVersionInfo(Application.ExecutablePath).FileVersion;
+
+            var Args = Environment.GetCommandLineArgs();
+            foreach (var Argument in Args)
+                if (Argument == "ShowVersion")
+                    ShowVersion(CurrentVersion);
+            textBox_Version.Text = CurrentVersion;
+        }
+
+        private void ShowVersion (string CurrentVersion)
+        {
+            MessageBox.Show("Версия программы " + CurrentVersion);
+            Environment.Exit(1);
+        }
+
+        private void Renovate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var FilePath = Path.GetDirectoryName(Application.ExecutablePath);
+                var FileName = Path.GetFileName(Application.ExecutablePath);
+                var Exten = Path.GetExtension(Application.ExecutablePath);
+                var OldFile = Path.GetFileNameWithoutExtension(Application.ExecutablePath) + "_old"+Exten;   
+                
+                if (File.Exists(String.Format("{0}\\new\\{1}", FilePath, FileName)))
+                    Process.Start(new ProcessStartInfo()
+                    {
+                        Arguments = String.Format("/c ping localhost & del {0} ", String.Format("{0}\\new\\{1}", FilePath, FileName)),
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                        CreateNoWindow = true,
+                        FileName = "cmd.exe"
+                    });
+
+                File.Move(Application.ExecutablePath, String.Format("{0}\\new\\{1}", FilePath, FileName));
+                File.Copy(String.Format("{0}\\new\\{1}", FilePath, FileName), Application.ExecutablePath);
+                File.Copy(Application.ExecutablePath, OldFile);
+
+
+
+                /*IntPtr hRes = BeginUpdateResourse(String.Format("{0}\\new\\{1}", FilePath, FileName), false);
+                bool result = false;
+                if (hRes == IntPtr.Zero) return;
+                result = UpdateResource(hRes, VERSION_INFO_ID, VERSION_NAME_ID, VERSION_LANG_ID, "Hello", 10);
+                if (!result) MessageBox.Show(String.Format("Error = "+Marshal.GetLastWin32Error()));
+                if (!EndUpdateResource(hRes, false)) MessageBox.Show(String.Format("Error = "+Marshal.GetLastWin32Error()));*/
+
+                
+
+                Process.Start(new ProcessStartInfo()
+                {
+                    Arguments = String.Format("/c ping localhost & del {0} & start {1}", OldFile, Application.ExecutablePath),
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    CreateNoWindow = true,
+                    FileName="cmd.exe"
+                });
+                Environment.Exit(1);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Ошибка: " + err.Message);
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
